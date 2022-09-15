5b15c9854ab13001d9e29c510c812b129deb50a2
LenSys <[omitted]>
2022-09-15 16:13:34 +0200

    added and modified routes and route names for index and create todos added create todo twig template

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/todos', name: 'app_todos.')]
 class TodosController extends AbstractController
 {
-    #[Route('/todos', name: 'app_todos')]
+    #[Route('', name: 'index')]
     public function index(): Response
     {
         return $this->render('todos/index.html.twig', [
+            'controller_name' => 'TodosController',
+        ]);
+    }
+
+
+    #[Route('/create', name: 'create')]
+    public function create():Response 
+    {
+        return $this->render('todos/create.html.twig', [
             'controller_name' => 'TodosController',
         ]);
     }
